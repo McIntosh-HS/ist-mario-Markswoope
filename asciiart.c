84768b872d4b39ff3fe5de29d5d87783d920ab4a
Marcus Swoope <2027swoope.marcus@fcboe.org>
2024-01-16 13:47:30 -0500

    Extra work

---
+++ asciiart.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	print("                              ")
+	print("                              ")
+	print("                              ")
+	print("    |          |              ")
+	print("    |          |              ")
+	print("    |          |              ")
+	print("    |          |              ")
+	print("  |               |           ")
+	print("   |             |            ")
+	print("    |           |             ")
+	print("     -----------              ")
+}
